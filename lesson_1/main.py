c3cccc5fb00e38f929ab059a41df7a2feef7e7eb
Dima Noskov <windn12@gmail.com>
2020-11-16 16:41:48 +0500

    lesson 1 corr

--- lesson_1/main.py
+++ lesson_1/main.py
@@ -1,9 +1,10 @@
-import requests
-import json
+import requests  # подключение библиотеки для выполнения запросов
+import json # подключение библиотеки для чтения формата json
 
-url = 'https://www.cbr-xml-daily.ru/daily_json.js'
-response = requests.get(url=url)
-data = json.loads(response.text)
-print(data)
+url = 'https://www.cbr-xml-daily.ru/daily_json.js' # адрес, куда отправляются запросы
+response = requests.get(url=url) # отправка запроса и запись ответа в переменную.
+data = json.loads(response.text) # чтение ответа от страницы, как текста, и запись в результата в переменную
+print(data) # вывод ответа от страницы на консоль
+# вывод названия определенной валюты и его курса
 print(f"Курс {data['Valute']['SGD']['Name']} - {data['Valute']['SGD']['Value']}")
 
